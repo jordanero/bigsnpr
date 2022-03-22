ce6aea2ddaa88f2b40bf21ecc8abb6936b5b3624
Jordan Rossen <jordanrossen@Jordans-MacBook-Pro.local>
2022-03-22 11:02:04 -0400

    further decomposed ssctpr loss function

--- src/ssctpr.cpp
+++ src/ssctpr.cpp
@@ -37,6 +37,29 @@ double loss_j(double beta_j, double one_plus_delta, double beta_hat_j,
   return (loss);
 }
 
+// the first element is \beta^T \tilde{X}^T \tilde{X}\beta
+// the second element is \delta \beta^T \beta
+// the third element is -2\beta^T r
+// the fourth element is 2\lambda ||\beta\\_1^1
+// the fifth element is \lambda_2\sum_t\sum_i(\beta_i - s_{ti})^2
+arma::vec termwise_loss_j(
+	double beta_j, double one_plus_delta, double beta_hat_j,
+        double dotprods_j, double lambda1, double lambda2,
+        double secondary_beta_hat_j) 
+{
+  arma::vec termwise_loss(5, arma::fill::zeros);
+
+  termwise_loss[0] = std::pow(beta_j, 2) ;
+  termwise_loss[0] += (-2) * (beta_j) * beta_hat_j;
+  termwise_loss[0] += (-2) * std::pow(beta_j, 2);
+  termwise_loss[1] = 2 * beta_j * dotprods_j;
+  termwise_loss[2] = std::pow(beta_j, 2) * (one_plus_delta - 1);
+  termwise_loss[3] = 2 * lambda1 * std::abs(beta_j);
+  termwise_loss[4] = lambda2 * std::pow(beta_j - secondary_beta_hat_j, 2);
+  
+return (termwise_loss);
+}
+
 double likelihood_j(double beta_j, double one_plus_delta, double beta_hat_j, 
                   double dotprods_j)
 {
@@ -96,8 +119,7 @@ List ssctpr(Environment corr,
   double one_plus_delta = 1 + delta;
   double gap0 = arma::dot(beta_hat, beta_hat);
   double loss = 0, likelihood = 0, penalty = 0;
-
-
+  arma::vec termwise_loss(5, arma::fill::zeros);
   
   int k = 0;
 
@@ -106,8 +128,9 @@ List ssctpr(Environment corr,
   std::ofstream myfile(logfile.c_str());
   if (logging) {
     loss = lambda2 * arma::sum(arma::pow(curr_beta - secondary_beta_hat, 2));
+    termwise_loss[4] = loss;
     penalty = loss;
-    myfile << "k, j, cor, secondary_cor, u_j, beta_hat_old, beta_hat_new, dotprods_j, likelihood, penalty, loss\n";
+    myfile << "k, j, cor, secondary_cor, u_j, beta_hat_old, beta_hat_new, dotprods_j, likelihood, penalty, loss, loss_term_1, loss_term_2, loss_term_3, loss_term_4, loss_term_5\n";
   }
 
   for (; k < maxiter; k++) {
@@ -137,6 +160,8 @@ List ssctpr(Environment corr,
         likelihood += (-1) * likelihood_j(curr_beta[j], one_plus_delta, beta_hat[j],
                                           dotprods[j]);
         penalty += (-1) * penalty_j(curr_beta[j], lambda1, lambda2, secondary_beta_hat[j]);
+	termwise_loss -= termwise_loss_j(curr_beta[j], one_plus_delta, beta_hat[j],
+                                         dotprods[j], lambda1, lambda2, secondary_beta_hat[j]);
       }
 
       double new_beta_j = ssctpr_update(u_j, lambda1, lambda2, 
@@ -161,11 +186,18 @@ List ssctpr(Environment corr,
         likelihood += likelihood_j(curr_beta[j], one_plus_delta, beta_hat[j],
                                    dotprods[j]);
         penalty += penalty_j(curr_beta[j], lambda1, lambda2, secondary_beta_hat[j]);
+	termwise_loss += termwise_loss_j(curr_beta[j], one_plus_delta, beta_hat[j],
+                                         dotprods[j], lambda1, lambda2, secondary_beta_hat[j]);
 
 
         myfile << double_to_string(likelihood) + ", ";
         myfile << double_to_string(penalty) + ", ";
-        myfile << double_to_string(loss);
+        myfile << double_to_string(loss) + ", ";
+        myfile << double_to_string(termwise_loss[0]) + ", ";
+        myfile << double_to_string(termwise_loss[1]) + ", ";
+        myfile << double_to_string(termwise_loss[2]) + ", ";
+        myfile << double_to_string(termwise_loss[3]) + ", ";
+        myfile << double_to_string(termwise_loss[4]);
 
         myfile << "\n";
       }
